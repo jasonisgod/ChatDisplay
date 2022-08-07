6bcb3b29e8da108c64c30e6d2914a3ef4fc96005
jasonisgod <jsonisgod@gmail.com>
2022-08-07 12:10:04 +0800

    comments, to_html(),

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,24 @@
+from sqlite3 import Timestamp
 import requests, json, sys
 from flask import Flask
 from flask_cors import CORS
 
 def d2j(d): return json.dumps(d)
 
+def to_html(comment):
+    html = f'<span class="author">{comment["author"]}</span> '
+    for e in comment['content']:
+        if e['type'] == 'text':
+            html += f'<span class="content text">{e["data"]}</span>'
+        if e['type'] == 'emoji':
+            html += f'<img src={e["url"]} style="width:16px">'
+    html += ' <br>'
+    return html
+
 def get_from_youtube_chat(vid, limit=30):
     try:
         url = 'https://studio.youtube.com/live_chat?is_popout=1&v=' + vid
-        headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
         response = requests.get(url, headers=headers)
         html = str(response.text)
         head = html.find('{"responseContext"')
@@ -15,27 +26,27 @@ def get_from_youtube_chat(vid, limit=30):
         json_str = html[head:tail]
         json_obj = json.loads(json_str)
         actions = json_obj['contents']['liveChatRenderer']['actions']
-        lines = []
+        comments = []
         for action in actions:
             try:
                 author = action['addChatItemAction']['item']['liveChatTextMessageRenderer']['authorName']['simpleText']
-                author = f'<span class="author">{author}</span>'
                 runs = action['addChatItemAction']['item']['liveChatTextMessageRenderer']['message']['runs']
-                content = ''
+                timestamp = action['addChatItemAction']['item']['liveChatTextMessageRenderer']['timestampUsec']
+                content = []
                 for run in runs:
                     if 'text' in run.keys(): 
-                        text = run['text']
-                        content += f'<span class="content">{text}</span>'
+                        data = run['text']
+                        content += [{'type':'text','data':data}]
                     if 'emoji' in run.keys(): 
                         url = run['emoji']['image']['thumbnails'][0]['url']
-                        content += f'<img src={url} style="width:16px">'
-                line = f'{author} {content} <br/>'
-                lines += [line]
+                        content += [{'type':'emoji','url':url}]
+                comment = {'timestamp': timestamp, 'author': author, 'content': content}
+                comments += [comment]
             except Exception as e:
                 pass
-        if len(lines) > limit:
-            lines = lines[-limit:]
-        return '\n'.join(lines)
+        if len(comments) > limit:
+            comments = comments[-limit:]
+        return comments
     except Exception as e:
         return 'Error'
 
@@ -50,8 +61,9 @@ CORS(app)
 
 @app.route("/api/data/<vid>/<limit>")
 def api_data(vid, limit):
-    data = get_from_youtube_chat(vid, int(limit))
-    return data
+    comments = get_from_youtube_chat(vid, int(limit))
+    htmls = [to_html(c) for c in comments]
+    return '\n'.join(htmls)
 
 print(f'HOST={HOST} PORT={PORT}')
 app.run(host=HOST, port=PORT)
