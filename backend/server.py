c96d7dcb9bc396e6be7514042f466dfa65bc515c
jasonisgod <jsonisgod@gmail.com>
2022-08-07 11:47:56 +0800

    UI fix

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
 import requests, json, sys
-from flask import Flask, request, render_template
+from flask import Flask
 from flask_cors import CORS
 
 def d2j(d): return json.dumps(d)
@@ -10,49 +10,31 @@ def get_from_youtube_chat(vid, limit=30):
         headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
         response = requests.get(url, headers=headers)
         html = str(response.text)
-        # print(html[:100])
-        # print(len(html))
         head = html.find('{"responseContext"')
         tail = html.find('};', head) + 1
         json_str = html[head:tail]
-        # print(head, tail)
-        # json_str = json_str.replace('\\','\\\\')
         json_obj = json.loads(json_str)
-        # print(json_obj)
         actions = json_obj['contents']['liveChatRenderer']['actions']
-        # actions = actions[-5:]
-        # print(actions)
         lines = []
         for action in actions:
             try:
-                # print(action.keys())
-                # print(action['addChatItemAction']['item'].keys())
-                # print(action['addChatItemAction']['item']['liveChatTextMessageRenderer'])
-                # continue
                 author = action['addChatItemAction']['item']['liveChatTextMessageRenderer']['authorName']['simpleText']
                 author = f'<span class="author">{author}</span>'
                 runs = action['addChatItemAction']['item']['liveChatTextMessageRenderer']['message']['runs']
-                comment = ''
+                content = ''
                 for run in runs:
                     if 'text' in run.keys(): 
                         text = run['text']
-                        comment += f'<span class="comment">{text}</span>'
-                        # comment += run['text']
+                        content += f'<span class="content">{text}</span>'
                     if 'emoji' in run.keys(): 
                         url = run['emoji']['image']['thumbnails'][0]['url']
-                        comment += f'<img src={url} style="width:16px">'
-                        # comment += run['emoji']['shortcuts'][0]
-                # print(author, comment)
-                line = f'{author} {comment} <br/>'
-                # print(line)
+                        content += f'<img src={url} style="width:16px">'
+                line = f'{author} {content} <br/>'
                 lines += [line]
             except Exception as e:
-                # raise e
-                # print(f'error: {e}')
                 pass
         if len(lines) > limit:
             lines = lines[-limit:]
-        lines += ['<style>.author{color:gray}</style>']
         return '\n'.join(lines)
     except Exception as e:
         return 'Error'
@@ -75,11 +57,11 @@ print(f'HOST={HOST} PORT={PORT}')
 app.run(host=HOST, port=PORT)
 
 
+
+'''
 # vid = 'PTUafmTYrsA'
 # data = get_from_youtube_chat(vid)
 # print('\n'.join(data))
-
-'''
 def remove_emoji(string):
     emoji_pattern = re.compile("["
         u"\U0001F600-\U0001F64F"  # emoticons
