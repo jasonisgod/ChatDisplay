0e43530e68fa217527120ccc4fcc476c3fd25026
jasonisgod <jsonisgod@gmail.com>
2022-08-07 19:35:06 +0800

    add jm_comment, timestamp,

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,18 @@
-from sqlite3 import Timestamp
-import requests, json, sys
-from flask import Flask
+import requests, json, sys, datetime
+from flask import Flask, request
 from flask_cors import CORS
 
+if len(sys.argv) != 2:
+    print('python3 server.py <PORT>')
+    exit()
+
+jm_comments = []
+
 def d2j(d): return json.dumps(d)
+def e2t(e): return datetime.datetime.fromtimestamp(e)
+def now(): return datetime.datetime.now()
+def t2s(t, format="%Y-%m-%d %H:%M:%S"): return t.strftime(format)
+def s2t(s, format="%Y-%m-%d %H:%M:%S"): return datetime.datetime.strptime(s,format)
 
 def to_html(comment):
     html = f'<span class="author">{comment["author"]}</span> '
@@ -15,7 +24,7 @@ def to_html(comment):
     html += ' <br>'
     return html
 
-def get_from_youtube_chat(vid, limit=30):
+def get_yt_comments(vid, limit=30):
     try:
         url = 'https://studio.youtube.com/live_chat?is_popout=1&v=' + vid
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
@@ -32,6 +41,7 @@ def get_from_youtube_chat(vid, limit=30):
                 author = action['addChatItemAction']['item']['liveChatTextMessageRenderer']['authorName']['simpleText']
                 runs = action['addChatItemAction']['item']['liveChatTextMessageRenderer']['message']['runs']
                 timestamp = action['addChatItemAction']['item']['liveChatTextMessageRenderer']['timestampUsec']
+                timestamp = e2t(int(timestamp[:-6]))
                 content = []
                 for run in runs:
                     if 'text' in run.keys(): 
@@ -48,22 +58,32 @@ def get_from_youtube_chat(vid, limit=30):
             comments = comments[-limit:]
         return comments
     except Exception as e:
-        return 'Error'
+        return [] #'Error'
 
-if len(sys.argv) != 2:
-    print('python3 server.py <PORT>')
-    exit()
+def add_jm_comments(author, content):
+    comment = {'timestamp': now(), 'author': author, 'content': [{'type':'text','data':content}]}
+    jm_comments.append(comment)
+    return True
 
 HOST = '0.0.0.0'
 PORT = int(sys.argv[1])
 app = Flask(__name__, template_folder='.')
 CORS(app)
 
-@app.route("/api/data/<vid>/<limit>")
+@app.route("/api/data/<vid>/<int:limit>")
 def api_data(vid, limit):
-    comments = get_from_youtube_chat(vid, int(limit))
+    yt_comments = get_yt_comments(vid, limit)
+    comments = (yt_comments + jm_comments)
+    comments = sorted(comments, key=lambda c: c['timestamp'])
+    comments = comments[-limit:]
     htmls = [to_html(c) for c in comments]
     return '\n'.join(htmls)
+
+@app.route("/api/add")
+def api_add():
+    author = request.args.get('author')
+    content = request.args.get('content')
+    return str(add_jm_comments(author, content))
 
 print(f'HOST={HOST} PORT={PORT}')
 app.run(host=HOST, port=PORT)
@@ -72,7 +92,7 @@ app.run(host=HOST, port=PORT)
 
 '''
 # vid = 'PTUafmTYrsA'
-# data = get_from_youtube_chat(vid)
+# data = get_yt_comments(vid)
 # print('\n'.join(data))
 def remove_emoji(string):
     emoji_pattern = re.compile("["
