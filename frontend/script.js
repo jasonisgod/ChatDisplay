dbbe15338dd721f3575f0739751dc0f203790493
jasonisgod <jsonisgod@gmail.com>
2022-08-07 20:32:18 +0800

    3 pages separated, DELAY, DOMAIN,

---
+++ frontend/script.js
@@ -0,0 +1,5 @@
+
+var DOMAIN_FRONTEND = 'http://127.0.0.1:5500/frontend'
+var DOMAIN_BACKEND = 'http://127.0.0.1:9005'
+// var DOMAIN_FRONTEND = 'http://jasonisgod.xyz/chatdisplay'
+// var DOMAIN_BACKEND = 'http://jasonisgod.xyz:9005'
